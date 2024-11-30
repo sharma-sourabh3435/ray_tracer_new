22ab33bd97a287e858e2f16a0f65e55ad088b371
sharma-sourabh3435 <sourabhsharma3435@gmail.com>
2024-11-30 12:56:12 -0400

    Add project files.

---
+++ ray_tracer.cpp
@@ -0,0 +1,12 @@
+﻿// ray_tracer.cpp : Defines the entry point for the application.
+//
+
+#include "ray_tracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
